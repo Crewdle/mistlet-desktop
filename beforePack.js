b7b0fb308a7d46c39df1f26f28961ed556344dd7
Vincent Lamanna <vincent@crewdle.com>
2024-09-26 23:18:21 -0400

    Fix building package

--- beforePack.js
+++ beforePack.js
@@ -11,6 +11,11 @@ module.exports = async (context) => {
     const platform = context.electronPlatformName || 'darwin'; // Default to darwin (macOS) if the platform is not defined
     const arch = context.arch === 1 ? 'x64' : 'arm64'; // Default to x64 if the architecture is not defined
 
+    execSync(`npx electron-rebuild --force --only=bufferutil,keytar,sharp,utf-8-validate`, {
+      cwd: context.packager.projectDir,
+      stdio: 'inherit' // This will output the command's output directly to the terminal
+    });
+
     execSync(`npx prebuild-install -r napi --platform=${platform} --arch=${arch}`, {
       cwd: datachannelPath,
       stdio: 'inherit' // This will output the command's output directly to the terminal
